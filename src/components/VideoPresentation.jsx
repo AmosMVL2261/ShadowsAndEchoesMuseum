9abedfe0c9b2361ef6da8cf527a2a72bc956e4d4
AmosMVL2261 <refi2261@gmail.com>
2023-06-21 12:24:43 -0600

    Box message added

--- src/components/VideoPresentation.jsx
+++ src/components/VideoPresentation.jsx
@@ -6,16 +6,25 @@ import frontMobile from "../assets/mobile.jpg"
 function VideoPresentation() {
   return (
     <>
-        <div className='videoDesktop'>
+        <div className='videoDesktop relative'>
             <video autoplay autoPlay muted playsinline playsInline loop id="myVideo">
                 <source src={video} type="video/mp4" />
             </video>
+            <div className='presentationText'>
+                <p>Welcome to the Shadows and Echoes Museum</p>
+            </div>
         </div>
-        <div className='imageTablet'>
+        <div className='imageTablet relative'>
             <img className='frontImage' src={front} alt="Museum cover image" />
+            <div className='presentationText'>
+            <p>Welcome to the Shadows and Echoes Museum</p>
+            </div>
         </div> 
-        <div className='imageMobile'>
+        <div className='imageMobile relative'>
             <img className='frontImage' src={frontMobile} alt="Museum cover image" />
+            <div className='presentationText'>
+            <p>Welcome to the Shadows and Echoes Museum</p>
+            </div>
         </div>
 
     </>
